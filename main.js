96aa0cecb335892e4030c1f7b4c03ea3b6a4cded
Kushal Harsora <kushalharsora13@gmail.com>
2025-01-24 12:42:21 +0530

    encryption and auth done

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 import crypto from "crypto";
 
 function encrypt_message(secret_message, secret_key) {
-    const hash = crypto.createHash("sha256").update(secret_key).digest("hex").slice(0, 32);
-    return hash;
+    const hash = crypto.createHash("sha256").update(secret_key).digest();
+    const iv = crypto.randomBytes(12);
+
+    const cipher = crypto.createCipheriv("aes-256-ccm", hash, iv, {
+        authTagLength: 16,
+    });
+
+    const cipher_text = Buffer.concat([
+        cipher.update(secret_message, "utf8"),
+        cipher.final(),
+    ]);
+
+    const authTag = cipher.getAuthTag();
+
+    return {
+        cipher_text: cipher_text.toString("hex"),
+        iv: iv.toString("hex"),
+        authTag: authTag.toString("hex"),
+    };
 }
 
 export function cipherVeil(secret_message, secret_key, cover_image) {
-    const hash = encrypt_message(secret_message, secret_key);
-    console.log(hash);
+    const { cipher_text, iv, authTag } = encrypt_message(secret_message, secret_key);
+    console.log("Cipher Text:", cipher_text);
+    console.log("IV:", iv);
+    console.log("Auth Tag:", authTag);
 }
