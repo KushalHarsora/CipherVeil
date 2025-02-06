436a265479ebaba530a2784625e575b827b2f0c7
Kushal Harsora <kushalharsora13@gmail.com>
2025-02-06 14:49:22 +0530

    Encryption Done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import crypto from "crypto";
+import { Jimp, JimpMime } from "jimp";
 
-function encrypt_message(secret_message, secret_key) {
+const encrypt_message = (secret_message, secret_key) => {
     const hash = crypto.createHash("sha256").update(secret_key).digest();
     const iv = crypto.randomBytes(12);
 
@@ -20,11 +21,43 @@ function encrypt_message(secret_message, secret_key) {
         iv: iv.toString("hex"),
         authTag: authTag.toString("hex"),
     };
-}
+};
 
-export function cipherVeil(secret_message, secret_key, cover_image) {
-    const { cipher_text, iv, authTag } = encrypt_message(secret_message, secret_key);
-    console.log("Cipher Text:", cipher_text);
-    console.log("IV:", iv);
-    console.log("Auth Tag:", authTag);
+const hexToBinary = (hex) => {
+    let binary = '';
+    for (let i = 0; i < hex.length; i++) {
+        const bin = parseInt(hex[i], 16).toString(2).padStart(4, '0');
+        binary += bin;
+    }
+    return binary;
+};
+
+const abstract_data = async (cipher_text, cover_imageBuffer) => {
+    const binary_data = hexToBinary(cipher_text);
+
+    try {
+        // Use the image buffer instead of a file path
+        const image = await Jimp.read(cover_imageBuffer);
+        let dataIndex = 0;
+
+        // Iterate over the image pixels to encode the binary data
+        image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+            if (dataIndex < binary_data.length) {
+                this.bitmap.data[idx] = (this.bitmap.data[idx] & 0b11111110) | parseInt(binary_data[dataIndex], 2);
+                dataIndex++;
+            }
+        });
+
+        const stego_imageBuffer = await image.getBuffer(JimpMime.png);
+        return stego_imageBuffer;
+
+    } catch (err) {
+        console.error("Error during steganography:", err);
+    }
+};
+
+export async function cipherVeil(secret_message, secret_key, cover_image) {
+    const { cipher_text } = encrypt_message(secret_message, secret_key);
+    const stego_imageBuffer = await abstract_data(cipher_text, cover_image);
+    return stego_imageBuffer;
 }
--- test.js
+++ test.js
@@ -1,4 +1,28 @@
-// only for dev test. Not included in final build
-import { cipherVeil } from "./main.js";
+import fs from 'fs';
+import { cipherVeil } from './main.js';
 
-cipherVeil("Hello", "alkdasjda", "adnslka");
+const secret_message = "This is a secret message";
+const secret_key = "supersecretkey";
+const cover_image_path = "./test.jpg";
+
+// Read the image file as a buffer
+const cover_imageBuffer = fs.readFileSync(cover_image_path);
+
+// Call cipherVeil to encode the message and return the image buffer
+cipherVeil(secret_message, secret_key, cover_imageBuffer)
+  .then((stego_imageBuffer) => {
+    // Save the stego image buffer to disk
+    fs.writeFile("stego_image.png", stego_imageBuffer, (err) => {
+      if (err) {
+        console.error("Error saving stego image:", err);
+      } else {
+        console.log("Stego image saved as stego_image.png");
+      }
+    });
+  })
+  .catch((err) => {
+    console.error("Error during steganography:", err);
+  });
+
+// decrypt
+
